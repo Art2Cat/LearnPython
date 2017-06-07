31706035a20d14426b4bc4fab7fcf29eca01b7c1
art2cat <yiming.whz@gmail.com>
2017-06-07 16:27:45 +0800

    add

---
+++ 2.Python function/decorator_n_partial.py
@@ -0,0 +1,47 @@
+# coding:utf-8
+import time
+import functools
+
+
+# def log(func):
+#     @functools.wraps(func)
+#     def wrapper(*args, **kw):
+#         print('call %s():' % func.__name__)
+#         return func(*args, **kw)
+#     return wrapper
+
+
+def log(text):
+    if isinstance(text, (str, float, int)):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kw):
+                print('%s %s():' % (text, func.__name__))
+                return func(*args, **kw)
+
+            return wrapper
+
+        return decorator
+    else:
+        @functools.wraps(text)
+        def wrapper(*args, **kw):
+            print('call %s():' % text.__name__)
+            return text(*args, **kw)
+
+        return wrapper
+
+
+@log("fuck")
+def now():
+    localtime = time.asctime(time.localtime(time.time()))
+    print(localtime)
+
+now()
+
+
+@log
+def now():
+    localtime = time.asctime(time.localtime(time.time()))
+    print(localtime)
+
+now()
---
+++ 2.Python function/functional_programming.py
@@ -0,0 +1,102 @@
+# coding:utf-8
+from functools import reduce
+
+
+def add(x, y, f):
+    return f(x) + f(y)
+
+
+print(add(10, -54, abs))
+
+
+def exponentiation(x, y):
+    return x ** y
+
+
+def multiply(x, y, f):
+    return f(x, y) * f(y, x)
+
+
+print(exponentiation(2, 10))
+print(multiply(2, 10, exponentiation))
+
+r = map(exponentiation, [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+print(list(r))
+
+r1 = reduce(exponentiation, [2, 2, 3, 4])
+
+print(r1)
+
+
+def add1(x, y):
+    return x + y
+
+
+def fn(x, y):
+    return x * 10 + y
+
+
+r2 = reduce(fn, [1, 2, 3, 4, 5, 6])
+
+print(r2)
+
+
+def str2int(s):
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+                '8': 8, '9': 9}[s]
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+
+s = "9527"
+
+print(str2int(s))
+
+
+def str2float(s):
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+                '8': 8, '9': 9}[s]
+    s1, s2 = s.split('.')
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s1)) + \
+        reduce(lambda x, y: x * 10 + y, map(char2num, s2)) / pow(10, len(s2))
+
+
+s = "9527.9527"
+
+print(str2float(s))
+
+
+def is_palindrome(n):
+    while n >= 10:
+        if str(n % 10) != str(n)[0]:
+            return False
+        n = int(str(n)[1:]) // 10
+    return True
+
+
+output = filter(is_palindrome, range(1, 1000))
+
+print(list(output))
+
+
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True))
+
+
+def by_name(t):
+    return t[0]
+
+
+def by_score(t):
+    return t[1]
+
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+print(by_name(L))
+L2 = sorted(L, key=by_name)
+L1 = sorted(L, key=by_score, reverse=True)
+print(L2)
+print(L1)
