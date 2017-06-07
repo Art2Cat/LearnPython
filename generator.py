849566ea5b8aa2fa0b5aa85993be778f7270f14d
art2cat <yiming.whz@gmail.com>
2017-06-07 13:09:57 +0800

    add new

---
+++ generator.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+# []包裹起来是列表解析式
+L = [x * x for x in range(10)]
+print(L)
+
+# ()包裹起来是生成器,可以用next()方法调用
+g = (x * x for x in range(10))
+
+for n in g:
+    print("n =", n)
+    print("next()= ", next(g))
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+
+for i in fib(6):
+    print(i)
